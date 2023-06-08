f324a842be79c74366ae6027cd7b6560ffc391de
Olaseni Bello <Olasenibello38@gmail.com>
2023-06-08 18:15:15 -0400

    Finished assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+// Assert equal function 1.0
+
+function assertEqual(actual,expected){
+    if(actual === expected){
+        console.log("Assertion passed 😄 ");
+    }else{
+        console.log("Assertion failed 😡 ");
+    }
+}
+
